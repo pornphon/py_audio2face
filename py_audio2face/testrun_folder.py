4151ec8dfa1e2451010ada84e1dbaf2f849a86aa
Pornphon Sakharin <pornphon@3dtv-tech.com>
2025-01-02 14:37:51 +0700

    update
    ใช้ file testrun_folder, single or stream

---
+++ py_audio2face/testrun_folder.py
@@ -0,0 +1,11 @@
+import py_audio2face as pya2f
+a2f = pya2f.Audio2Face()
+
+
+### Generate animation for audio files:
+
+
+# Generate animation for a single audio file
+a2f.audio2face_folder(input_folder="X:/VanillaCGs_Projs/2024/884_VirtualPresenter/03_PostProduction/07_Sound/", output_folder='X:/VanillaCGs_Projs/2024/884_VirtualPresenter/03_PostProduction/07_Sound/output', fps=60)
+
+
